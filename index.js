8c5bde7ce4ef5c28e49b68cb025af88992d1e910
nuzzesick <mati280341@gmail.com>
2021-04-12 22:23:39 -0300

    more fixes

--- index.js
+++ index.js
@@ -17,6 +17,6 @@ app.get('/torrent', (req, res) => {
   });
 });
 
-app.listen(port, () => {
+app.listen(process.env.PORT || port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
