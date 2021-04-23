7cb8a0c48b3b834e2673967e1967fd43e2a2ad35
nuzzesick <mati280341@gmail.com>
2021-04-23 01:58:24 -0300

    version 1

--- index.js
+++ index.js
@@ -1,19 +1,42 @@
 const WebTorrent = require('webtorrent-hybrid');
+const cors = require('cors');
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 8080;
+
+app.use(cors());
+
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
   res.send('App running');
 });
 
-app.get('/torrent', (req, res) => {
-  const client = new WebTorrent();
-  const magnetURI = 'https://yts.mx/torrent/download/BB43CF1DC5B200BA37679DB96375A8190D933C2E';
-  client.add(magnetURI, { path: 'torrents' }, (torrent) => {
-    torrent.on('done', () => {
-      console.log('torrent download finished');
+app.use('/download', (req, res, next) => {
+  const startTorrent = () => {
+    return new Promise ((resolve) => {
+      const client = new WebTorrent();
+      const { hash } = req.query;
+      client.add(hash, { path: 'public/torrents' }, (torrent) => {
+        console.log('Download started...\n');
+        console.log('You can watch the movie while is downloading!\n');
+        client.on('torrent', () => {
+          const file = torrent.files.find((f) => f.name.endsWith('.mp4'));
+          const { name, path } = file;
+          res.json({ name, path });
+          resolve();
+        })
+      });
     });
+  };
+  startTorrent().then(() => { next(); })
+});
+
+app.get('download', (req, res) => {
+  const client = new WebTorrent();
+  client.seed('public/torrents', (torrent) => {
+    console.log('Client is seeding ' + torrent.magnetURI);
+    res.send('finish');
   });
 });
 
